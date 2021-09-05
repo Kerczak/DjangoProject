dce8cc60378675aa906d304daa2d5522be195179
Kerczak <karolzielinski94@gmail.com>
2021-09-05 12:21:40 +0200

    Added password reset

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -13,26 +13,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
 
-from viewer.views import MoviesView, MovieCreateView, MovieUpdateView, MovieDeleteView, SubmittableLoginView
+from viewer.views import MoviesView, MovieCreateView, \
+    MovieUpdateView, MovieDeleteView
 from viewer.models import Genre, Movie
-from django.contrib.auth.views import LoginView
 
 from viewer.views import generate_demo
+
+from accounts.views import SubmittableLoginView, SubmittablePasswordChangeForm
+
+from django.contrib.auth import views
 
 admin.site.register(Genre)
 admin.site.register(Movie)
 
 urlpatterns = [
     path('accounts/login/', SubmittableLoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
+    path('password_change/', SubmittablePasswordChangeForm.as_view(),
+         name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    path('password_reset/', views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done', views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
     path('admin/', admin.site.urls),
     path('', MoviesView.as_view(), name='index'),
     path('demo', generate_demo, name='demo'),
     path('movie/create', MovieCreateView.as_view(), name='movie_create'),
     path('movie/update/<pk>', MovieUpdateView.as_view(), name='movie_update'),
-    path('movie/delete/<pk>', MovieDeleteView.as_view(), name='movie_delete')
+    path('movie/delete/<pk>', MovieDeleteView.as_view(), name='movie_delete'),
 ]
